a1b4c9d1cfcf30630f6ccf150092db24130f60ac
Mo-Greene <13blueboy13@daum.net>
2022-06-27 11:38:57 +0900

    feat @EnableJpaRepositories

--- src/main/java/com/example/game/GameApplication.java
+++ src/main/java/com/example/game/GameApplication.java
@@ -2,9 +2,11 @@ package com.example.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class GameApplication {
 
     public static void main(String[] args) {SpringApplication.run(GameApplication.class, args);
