b82f32ba94995f6c0853fa22e3d51c6ef989fd20
Mo-Greene <13blueboy13@daum.net>
2022-07-29 01:46:00 +0900

    add: cors필터, 카카오서비스 redirect uri 수정

--- src/main/java/com/example/game/security/filter/CorsConfig.java
+++ src/main/java/com/example/game/security/filter/CorsConfig.java
@@ -17,6 +17,8 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:3000");           // 파이어베이스 생성시 한줄더 추가
         config.addAllowedOrigin("http://62442.s3-website.ap-northeast-2.amazonaws.com");
         config.addAllowedOrigin("http://drunkenwizardtest.s3-website.ap-northeast-2.amazonaws.com/");
+        config.addAllowedOrigin("http://drunken-wizard.com");
+        config.addAllowedOrigin("https://drunken-wizard.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("*");
