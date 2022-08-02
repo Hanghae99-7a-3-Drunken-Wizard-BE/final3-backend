c6a4025b6ab930ac927d72cf28a832f3fff5e33c
Mo-Greene <13blueboy13@daum.net>
2022-08-02 20:48:31 +0900

    add: 무중단배포 설정추가

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,27 @@
+# run_new_was.sh
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+fi
+
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+JAR_NAME=$(ls -tr /home/ubuntu/drunken-wizard/build/libs/*.jar | tail -n 1)
+echo "> JAR Name: $JAR_NAME"
+chmod +x $JAR_NAME
+nohup java -jar -Dserver.port=${TARGET_PORT} $JAR_NAME > /home/ubuntu/nohup.out 2>&1 &
+echo "> Now new WAS runs at ${TARGET_PORT}."
+exit 0
